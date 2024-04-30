ea3de9b4cf5bbbf8250045ffff36f54ed5dca59f
Jose Garcia <jose.garcia051@gmail.com>
2024-04-29 21:31:23 -0400

    updated keymappings for neo vim

---
+++ nvim/lua/user/keymaps.lua
@@ -0,0 +1,15 @@
+-- Space is my leader.
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Relesect visual selection after indenting.
+vim.keymap.set('v', '<', '<gv')
+vim.keymap.set('v', '>', '>gv')
+
+-- Maintain the cursor position when yanking a visual selection.
+vim.keymap.set('v', 'y', 'myy`y')
+
+-- Paste replace visual selection without copying it.
+vim.keymap.set('v', 'p', '"_dP')
+
+-- Easy insetion of a trailing ; or , from inser mode.
