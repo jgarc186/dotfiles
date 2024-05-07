9c1601f0beeb56eed71dad2c908912ad94506e66
Jose Garcia <jose.garcia051@gmail.com>
2024-05-07 09:25:38 -0400

    working through file navigation

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -14,6 +14,11 @@ local packer_bootstrap = ensure_packer()
 require('packer').reset()
 require('packer').init({
 	compile_path = vim.fn.stdpath('data')..'/site/plugin/packer_compiled.lua',
+    display = {
+        open_fn = function()
+            return require('packer.util').float({ border = 'solid' })
+        end,
+    }
 })
 
 local use = require('packer').use
@@ -22,8 +27,40 @@ local use = require('packer').use
 -- Packer manage itself.
 use('wbthomason/packer.nvim')
 
+-- Themes
+use({
+    'catppuccin/nvim',
+    as = 'catppuccin',
+    config = function()
+        vim.cmd('colorscheme catppuccin-macchiato')
+    end,
+})
+
 -- Commenting support
-use('tpope/vim-commentary')
+ use('tpope/vim-commentary')
+
+-- Fuzzy finder
+use({
+    'nvim-telescope/telescope.nvim',
+    requires = {
+        'nvim-lua/plenary.nvim',
+        'kyazdani42/nvim-web-devicons',
+        'nvim-telescope/telescope-live-grep-args.nvim',
+        { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
+    },
+    config = function()
+        require('user/plugins/telescope')
+    end,
+})
+
+-- File tree sidebar
+use({
+    'kyazdani42/nvim-tree.lua',
+    requires = 'kyazdani42/nvim-web-devicons',
+    config = function()
+        require('nvim-tree').setup()
+    end,
+})
 
 -- Automatically set up your configuration after cloning packer.nvim
 -- Put this at the end after all plugins
