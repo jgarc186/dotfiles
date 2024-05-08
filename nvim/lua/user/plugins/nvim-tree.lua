8804fcd87225e03571e95117c8a20c5bb3bd7d63
Jose Garcia <jose.garcia051@gmail.com>
2024-05-07 23:36:44 -0400

    better responsaiveness to the tree when the window adjust

--- nvim/lua/user/plugins/nvim-tree.lua
+++ nvim/lua/user/plugins/nvim-tree.lua
@@ -12,7 +12,24 @@ require('nvim-tree').setup({
         indent_markers = {
             enable = true
         }
-    }
+    },
+ })
+
+local tree_api = require("nvim-tree")
+local tree_view = require("nvim-tree.view")
+
+vim.api.nvim_create_augroup("NvimTreeResize", {
+  clear = true,
+})
+
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+  group = "NvimTreeResize",
+  callback = function()
+    if tree_view.is_visible() then
+      tree_view.close()
+      tree_api.open()
+    end
+  end
 })
 
 vim.keymap.set('n', '<leader>1', ':NvimTreeFindFileToggle<CR>')
