2ee998707f9a6b037e206addc0fe2035c4ebe6b3
Jose Garcia <jose.garcia051@gmail.com>
2024-09-21 21:35:06 -0400

    added support for python and ruby and disabled syntaxed highlight feature

--- nvim/lua/user/plugins/lspconfig.lua
+++ nvim/lua/user/plugins/lspconfig.lua
@@ -2,25 +2,25 @@
 require('mason').setup()
 require('mason-lspconfig').setup({ automatic_installation = true })
 
-
 -- PHP:
 require('lspconfig').intelephense.setup({})
 
 -- React, Vue, TS, & JS
 require('lspconfig').volar.setup({
-  filetypes = {'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json'}
+  filetypes = {'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue'},
 })
 require('lspconfig').tsserver.setup({
-    filetypes = {
-        'typescript',
-        'typescriptreact',
-        'typescript.tsx',
-    },
+    filetypes = { 'typescript', 'typescriptreact', 'typescript.tsx' },
     cmd = { 'typescript-language-server', '--stdio' }
 })
 
---require('lspconfig').vuels.setup({})
+-- Python language server
+require('lspconfig').basedpyright.setup({})
 
+-- Ruby Language server
+require('lspconfig').ruby_lsp.setup({})
+
+-- tailwind css Language Server
 require('lspconfig').tailwindcss.setup({})
 
 -- mkeymaps
--- nvim/lua/user/plugins/treesitter.lua
+++ nvim/lua/user/plugins/treesitter.lua
@@ -1,22 +1,22 @@
-require('nvim-treesitter.configs').setup({
-    ensure_installed = 'all',
-    highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = true
-    },
-    context_commentsring = {
-        enable = true
-    },
-    textobjects = {
-        select = {
-            enable = true,
-            lookahead = true,
-            keymap = {
-                ['if'] = '@function.inner',
-                ['af'] = '@function.outer',
-                ['ia'] = '@paramter.inner',
-                ['aa'] = '@paramter.outer'
-            }
-        }
-    }
-})
+-- require('nvim-treesitter.configs').setup({
+--    ensure_installed = 'all',
+--    highlight = {
+--        enable = true,
+--        additional_vim_regex_highlighting = true
+--    },
+--    context_commentsring = {
+--        enable = true
+--    },
+--    textobjects = {
+--        select = {
+--            enable = true,
+--            lookahead = true,
+--            keymap = {
+--                ['if'] = '@function.inner',
+--                ['af'] = '@function.outer',
+--                ['ia'] = '@paramter.inner',
+ --               ['aa'] = '@paramter.outer'
+--            }
+--        }
+--    }
+-- })
