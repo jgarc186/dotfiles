159ddca162eda77c710591d4f3f0fb9c3829abca
Jose Garcia <jose.garcia@thinkiq.com>
2024-11-07 14:30:37 -0500

    added a hacky solution to reload the options files everytime we open a new buffer

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,3 +2,8 @@ require('user/plugins')
 require('user/options')
 require('user/keymaps')
 
+-- Automatically source options.lua on BufEnter
+vim.api.nvim_create_autocmd('BufEnter', {
+    pattern = '*',
+    command = 'source ~/developer/josegarcia/dotfiles/nvim/lua/user/options.lua'
+})
