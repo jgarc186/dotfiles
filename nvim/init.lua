cf4e9fb6888fc7057f525731426f5b0eef97d0c5
Jose Garcia <jose.garcia@thinkiq.com>
2024-12-19 08:54:12 -0500

    updates to the none-ls plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,7 +4,7 @@ require('user/keymaps')
 
 -- Auto format C# files on save
 vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*.cs",
+    pattern = "*.cs, *.js, *.ts, *.jsx, *.tsx, *.vue, *.json, *.php",
     callback = function()
         vim.lsp.buf.format({ async = false })
     end,
