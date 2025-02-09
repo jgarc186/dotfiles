c9d73df5150318280863fe68e8d976b282faf5d3
Jose Garcia <jose.garcia051@gmail.com>
2025-02-08 20:54:02 -0500

    replace packer with lazy

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,6 @@
+-- require('user/lazy')
 require('user/plugins')
 require('user/options')
-require('user/keymaps')
 
 -- Auto format C# files on save
 vim.api.nvim_create_autocmd("BufWritePre", {
--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -1,211 +1,178 @@
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
   end
-  return false
 end
+vim.opt.rtp:prepend(lazypath)
 
-local packer_bootstrap = ensure_packer()
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+require('user/keymaps')
 
-require('packer').reset()
-require('packer').init({
-	compile_path = vim.fn.stdpath('data')..'/site/plugin/packer_compiled.lua',
-    display = {
-        open_fn = function()
-            return require('packer.util').float({ border = 'solid' })
+-- Setup lazy.nvim
+require("lazy").setup({
+
+    -- Themes
+    {
+        'catppuccin/nvim',
+        as = 'catppuccin',
+        config = function()
+            -- options: latte, frappe, macchiato, mocha
+           vim.cmd('colorscheme catppuccin-mocha')
+
+            vim.api.nvim_set_hl(0, 'FloatBorder', {
+                fg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
+                bg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
+            })
+
+            -- Make the cursor line invisible
+            vim.api.nvim_set_hl(0, 'CursorLinmeBg', {
+                fg = vim.api.nvim_get_hl_by_name('CursorLine', true).background,
+                bg = vim.api.nvim_get_hl_by_name('CursorLine', true).background,
+            })
+            
+            vim.api.nvim_set_hl(0, 'StatusLineNonText', {
+                fg = vim.api.nvim_get_hl_by_name('NonText', true).foreground,
+                bg = vim.api.nvim_get_hl_by_name('StatusLine', true).background,
+            })
+
+            vim.api.nvim_set_hl(0, 'NvimTreeIndentMarker', { fg = '#30323E' })
         end,
-    }
-})
-
-local use = require('packer').use
-
--- My plugins here
--- Packer manage itself.
-use('wbthomason/packer.nvim')
-
--- Themes
-use({
-    'catppuccin/nvim',
-    as = 'catppuccin',
-    config = function()
-        -- options: latte, frappe, macchiato, mocha
-       vim.cmd('colorscheme catppuccin-mocha')
-
-        vim.api.nvim_set_hl(0, 'FloatBorder', {
-            fg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
-            bg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
-        })
-
-        -- Make the cursor line invisible
-        vim.api.nvim_set_hl(0, 'CursorLinmeBg', {
-            fg = vim.api.nvim_get_hl_by_name('CursorLine', true).background,
-            bg = vim.api.nvim_get_hl_by_name('CursorLine', true).background,
-        })
-        
-        vim.api.nvim_set_hl(0, 'StatusLineNonText', {
-            fg = vim.api.nvim_get_hl_by_name('NonText', true).foreground,
-            bg = vim.api.nvim_get_hl_by_name('StatusLine', true).background,
-        })
-
-        vim.api.nvim_set_hl(0, 'NvimTreeIndentMarker', { fg = '#30323E' })
-    end,
-})
-
--- Github Copilot
-use('github/copilot.vim')
-
--- Commenting support
-use('tpope/vim-commentary')
-
--- Fuzzy finder
-use({
-    'nvim-telescope/telescope.nvim',
-    requires = {
-        'nvim-lua/plenary.nvim',
-        'kyazdani42/nvim-web-devicons',
-        'nvim-telescope/telescope-live-grep-args.nvim',
-        { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
     },
-    config = function()
-        require('user/plugins/telescope')
-    end,
-})
 
--- File tree sidebar
-use({
-    'kyazdani42/nvim-tree.lua',
-    requires = 'kyazdani42/nvim-web-devicons',
-    config = function()
-        require('user/plugins/nvim-tree')
-    end,
-})
+    -- Github Copilot
+    'github/copilot.vim',
 
--- A status line.
-use({
-    'nvim-lualine/lualine.nvim',
-    requires = 'kyazdani42/nvim-web-devicons',
-    config = function()
-        require('user/plugins/lualine')
-    end
-})
+    -- Commenting support
+    'tpope/vim-commentary',
 
--- Display Buffers as tabs
--- use({
---     'akinsho/bufferline.nvim',
---     requires = 'kyazdani42/nvim-web-devicons',
---     after = 'catppuccin',
---     config = function()
---         require('user/plugins/bufferline')
---     end
--- })
-
--- Display indentation lines.
--- use({
---     'lukas-reineke/indent-blankline.nvim',
---     config = function()
---         require('user/plugins/indent-blankline')
---     end
--- })
-
-use({
-  'echasnovski/mini.indentscope',
-  version = '*',
-  config = function()
-    require('mini.indentscope').setup({})
-  end
-})
-
--- git plugin
-use({
-    'lewis6991/gitsigns.nvim',
-    config = function()
-        require('gitsigns').setup()
-    end
-})
-
-use({
-    'voldikss/vim-floaterm',
-    config = function()
-        vim.g.floaterm_wintype = 'vsplit'
-        --vim.g.floaterm_height = 0.2
-        vim.g.floaterm_width = 0.2
-        vim.keymap.set('n', '<leader>`', ':FloatermToggle<CR>')
-        vim.keymap.set('t', '<leader>`', '<C-\\><C-n>:FloatermToggle<CR>')
-    end
-})
-
--- Improive syntax highlighting
--- use({
---     'nvim-treesitter/nvim-treesitter',
---     run = function()
---         require('nvim-treesitter.install').update({ with_sync = true })
---     end,
---     requires = {
---         'JoosepAlviste/nvim-ts-context-commentstring',
---         'nvim-treesitter/nvim-treesitter-textobjects'
---     },
---     config = function()
---         require('user.plugins.treesitter')
---     end
--- })
-
--- Language server protocol.
-use({
-    'neovim/nvim-lspconfig',
-    requires = {
-        { 
-            'williamboman/mason.nvim', 
-            build = ':MasonUpdate' 
+    -- Fuzzy finder
+    {
+        'nvim-telescope/telescope.nvim',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'kyazdani42/nvim-web-devicons',
+            'nvim-telescope/telescope-live-grep-args.nvim',
+            { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
         },
-        'williamboman/mason-lspconfig.nvim',
-        'b0o/schemastore.nvim',
+        config = function()
+            require('user/plugins/telescope')
+        end,
     },
-    config = function()
-        require('user/plugins/lspconfig')
-    end
+
+    -- File tree sidebar
+    {
+        'kyazdani42/nvim-tree.lua',
+        dependencies = 'kyazdani42/nvim-web-devicons',
+        config = function()
+            require('user/plugins/nvim-tree')
+        end,
+    },
+
+    -- A status line.
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = 'kyazdani42/nvim-web-devicons',
+        config = function()
+            require('user/plugins/lualine')
+        end
+    },
+
+    {
+      'echasnovski/mini.indentscope',
+      version = '*',
+      config = function()
+        require('mini.indentscope').setup({})
+      end
+    },
+
+    -- git plugin
+    {
+        'lewis6991/gitsigns.nvim',
+        config = function()
+            require('gitsigns').setup()
+        end
+    },
+
+    {
+        'voldikss/vim-floaterm',
+        config = function()
+            vim.g.floaterm_wintype = 'vsplit'
+            --vim.g.floaterm_height = 0.2
+            vim.g.floaterm_width = 0.2
+            vim.keymap.set('n', '<leader>`', ':FloatermToggle<CR>')
+            vim.keymap.set('t', '<leader>`', '<C-\\><C-n>:FloatermToggle<CR>')
+        end
+    },
+
+    -- Improive syntax highlighting
+    -- use({
+    --     'nvim-treesitter/nvim-treesitter',
+    --     build = function()
+    --         require('nvim-treesitter.install').update({ with_sync = true })
+    --     end,
+    --     dependencies = {
+    --         'JoosepAlviste/nvim-ts-context-commentstring',
+    --         'nvim-treesitter/nvim-treesitter-textobjects'
+    --     },
+    --     config = function()
+    --         require('user.plugins.treesitter')
+    --     end
+    -- })
+
+    -- Language server protocol.
+    {
+        'neovim/nvim-lspconfig',
+        dependencies = {
+            { 
+                'williamboman/mason.nvim', 
+                build = ':MasonUpdate' 
+            },
+            'williamboman/mason-lspconfig.nvim',
+            'b0o/schemastore.nvim',
+        },
+        config = function()
+            require('user/plugins/lspconfig')
+        end
+    },
+
+    -- Formatter
+    {
+        "nvimtools/none-ls.nvim", -- Correct repository name
+        dependencies = { 
+            "nvim-lua/plenary.nvim" -- Ensure correct repository name for plenary.nvim
+        },
+        config = function()
+            require('user.plugins.none-ls') -- Ensure the path matches your configuration file
+        end
+    },
+
+    -- Autocompletion
+    {
+        'hrsh7th/nvim-cmp',
+        dependencies = {
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-nvim-lsp-signature-help',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'L3MON4D3/LuaSnip',
+            'saadparwaiz1/cmp_luasnip',
+            'onsails/lspkind-nvim',
+        },
+        config = function()
+            require('user/plugins/cmp')
+        end,
+    },
 })
 
--- Formatter
-use({
-    "nvimtools/none-ls.nvim", -- Correct repository name
-    requires = { 
-        "nvim-lua/plenary.nvim" -- Ensure correct repository name for plenary.nvim
-    },
-    config = function()
-        require('user.plugins.none-ls') -- Ensure the path matches your configuration file
-    end
-})
-
--- Autocompletion
-use({
-    'hrsh7th/nvim-cmp',
-    requires = {
-        'hrsh7th/cmp-nvim-lsp',
-        'hrsh7th/cmp-nvim-lsp-signature-help',
-        'hrsh7th/cmp-buffer',
-        'hrsh7th/cmp-path',
-        'L3MON4D3/LuaSnip',
-        'saadparwaiz1/cmp_luasnip',
-        'onsails/lspkind-nvim',
-    },
-    config = function()
-        require('user/plugins/cmp')
-    end,
-})
-
--- Automatically set up your configuration after cloning packer.nvim
--- Put this at the end after all plugins
-if packer_bootstrap then
-	require('packer').sync()
-end
-
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> 
-  augroup end
-]])
