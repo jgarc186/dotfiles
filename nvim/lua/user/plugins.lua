e08f47e2006c4ce8b4794c6fd5fd6e22acee9928
Jose Garcia <jose.garcia051@gmail.com>
2025-05-25 11:09:07 -0400

    added new markdown preview plugin

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -183,5 +183,12 @@ require("lazy").setup({
             require('user/plugins/cmp')
         end,
     },
+
+    -- Markdown preview
+    {
+        'MeanderingProgrammer/render-markdown.nvim',
+        dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+        opts = {},
+    }
 })
 
