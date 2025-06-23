6a48f0aafb356d2c2552fa7ae686f049b99b4483
Jose Garcia <jose.garcia@tthinkiq.com>
2025-06-23 10:13:28 -0400

    updated some of the config from mvim-tree

--- nvim/lua/user/plugins/nvim-tree.lua
+++ nvim/lua/user/plugins/nvim-tree.lua
@@ -1,39 +1,34 @@
-require('nvim-tree').setup({
-    view = {
-        width = 50,
-        side = 'right',
+require("nvim-tree").setup({
+  view = {
+    width = 50,
+    side = "right",
+  },
+  git = {
+    ignore = false,
+  },
+  renderer = {
+    group_empty = true,
+    icons = {
+      show = {
+        folder_arrow = false,
+      },
     },
-    git = {
-        ignore = false,
-    },
-    renderer = {
-        group_empty = true,
-        icons = {
-            show = {
-                folder_arrow = false
-            }
-        },
-        indent_markers = {
-            enable = true
-        }
-    },
- })
 
-local tree_api = require("nvim-tree")
-local tree_view = require("nvim-tree.view")
-
-vim.api.nvim_create_augroup("NvimTreeResize", {
-  clear = true,
+    indent_markers = {
+      enable = true,
+    },
+  },
 })
 
-vim.api.nvim_create_autocmd({ "VimResized" }, {
+-- Safely reload tree on resize (without accessing internal APIs)
+vim.api.nvim_create_augroup("NvimTreeResize", { clear = true })
+
+vim.api.nvim_create_autocmd("VimResized", {
   group = "NvimTreeResize",
   callback = function()
-    if tree_view.is_visible() then
-      tree_view.close()
-      tree_api.open()
-    end
-  end
+    vim.cmd("NvimTreeRefresh")  -- safer than using removed internals
+  end,
 })
 
-vim.keymap.set('n', '<leader>1', ':NvimTreeFindFileToggle<CR>')
+vim.keymap.set("n", "<leader>1", ":NvimTreeFindFileToggle<CR>", { noremap = true, silent = true })
+
