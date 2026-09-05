2eccd9c913d0a26b167aa04becf36792bd7f2957
Jose Garcia <jose.garcia051@gmail.com>
2026-09-04 20:02:03 -0400

    testing the hooks

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,6 @@
-require('user/plugins')
-require('user/options')
-
--- Colours come from matugen, shared with the rest of the desktop; this also
--- watches the generated palette so a light/dark switch reaches a running nvim.
-require('user/theme').setup()
+require("user/plugins")
+require("user/options")
+require("user/theme").setup()
 
 vim.o.termsync = false
 
